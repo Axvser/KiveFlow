a494593d206ddc53d78a1825099888de28dcd59c
俊杰 冯 <Axvser@outlook.com>
2026-01-23 00:48:20 +0800

    添加项目文件。

---
+++ KiveFlow/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using VeloxDev.Core.MVVM;
+
+namespace KiveFlow.ViewModels;
+
+public partial class MainViewModel
+{
+    [VeloxProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ KiveFlow/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KiveFlow.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
